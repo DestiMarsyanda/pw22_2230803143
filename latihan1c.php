898a4842e77d2255ca3030d71183f50dd0f93d0d
DestiMarsyanda <145082903+DestiMarsyanda@users.noreply.github.com>
2023-12-15 14:21:05 +0700

    Update latihan1c.php
    Diupdate, dikarenakan ada beberapa typo sebelumnya.

--- latihan1c.php
+++ latihan1c.php
@@ -1,11 +1,12 @@
-<DOCTYPE php>
+<DOCTYPE html>
     <html lang="en">
         <head>
             <style>
-                th,td{
-                    border:ipx solid black;
+                th, td{
+                    border:1px solid black;
                     margin:30px;
                     height:20px;
+                    width:20px;
                     padding:10px;
                     text-align:center;
                 }
@@ -16,9 +17,9 @@
                         <?php
                         $a="A";
                         $b="B";
-                        $c="c";
+                        $c="C";
                         echo "<tr>";
-                        echo "<tr>".$a."</td>";
+                        echo "<td>".$a."</td>";
                         echo "</tr>";
                         echo "<tr>";
                         echo "<td>".$a."</td>";
@@ -28,7 +29,7 @@
                         echo "<td>".$a."</td>";
                         echo "<td>".$b."</td>";
                         echo "<td>".$c."</td>";
-                        echo "<tr>";
+                        echo "</tr>";
                         ?>
                         </table>
             </body>
