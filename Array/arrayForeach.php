9ce41d1e671a2b3011d91a8ae9e40802bc9cad48
DestiMarsyanda <145082903+DestiMarsyanda@users.noreply.github.com>
2023-12-16 19:10:29 +0700

    Add files via upload

---
+++ Array/arrayForeach.php
@@ -0,0 +1,20 @@
+<?php
+$nama = ["Ahmad", "Budi", "Chika", "Dhini", "Erwin"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset= "UTF-8">
+        <title>Looping for array</title>
+    </head>
+    <body>
+        <?php
+        foreach ($nama as $absen) { // Foreach akan otomatis menampilkan semua isi array
+            echo "<li>$absen</li>";
+        }
+
+        ?>
+    </body>
+</html>
